516c09ae81191090c543da438606eb75a353a5b1
Wesley Shih <wesley.shih@daqri.com>
2014-07-22 19:10:10 -0700

    adding ripple scripts

---
+++ ripple.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ripple : MonoBehaviour {
+
+	public int rows;
+	public int cols;
+
+	Mesh mesh;
+	float[][] heights;
+
+	// Use this for initialization
+	void Start () {
+		var meshFilter = GetComponent<MeshFilter>();
+		mesh = meshFilter.mesh;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
